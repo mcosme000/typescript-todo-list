728090e9b4f6359bcd6536849f438da1b87421cd
Maria Cosmen <mcosme00@gmail.com>
2023-09-14 17:41:43 +0900

    Basic todo list

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hello World");
+var form = document.querySelector("form");
+var button = document.getElementById("submit");
+var input = document.getElementById("input");
+var list = document.getElementById("tasks-container");
+var addNewTask = function (task) {
+    var newTask = document.createElement("LI");
+    newTask.innerText = task;
+    var checkbox = document.createElement("INPUT");
+    checkbox.type = "checkbox";
+    newTask.append(checkbox);
+    list === null || list === void 0 ? void 0 : list.appendChild(newTask);
+};
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    addNewTask(input.value);
+    input.value = "";
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const form = document.querySelector("form")!;
+const button = document.getElementById("submit")!;
+const input = document.getElementById("input")! as HTMLInputElement;
+const list = document.getElementById("tasks-container");
+
+const addNewTask = (task: string) => {
+  let newTask = document.createElement("LI") as HTMLLIElement;
+  newTask.innerText = task
+  let checkbox = document.createElement("INPUT") as HTMLInputElement;
+  checkbox.type = "checkbox";
+  newTask.append(checkbox);
+  list?.appendChild(newTask);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addNewTask(input.value)
+  input.value = ""
+})
